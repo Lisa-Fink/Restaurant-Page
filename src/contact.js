853bb3fefd3b05c0ef1c1b2ac00fbabb87c82d86
Lisa Fink <lisadfink@gmail.com>
2022-05-12 00:37:56 -0400

    Added a contact form and styling for contact tab

--- src/contact.js
+++ src/contact.js
@@ -1,15 +1,36 @@
 import { heading } from "./home";
 
 function contactForm() {
-    const inputArr = [fullname, phone, email, message, submit];
+    const inputArr = ['Full Name*', 'Phone*', 'Email*', 'Message*'];
+    const form = document.createElement('form');
+    form.id = 'form'
+
+    inputArr.forEach(input => {
+        const formInput = input != 'Message' ? document.createElement('input') :
+            document.createElement('textarea');
+        formInput.placeholder = input;
+        input == 'Phone' ? (formInput.type = 'tel', 
+            formInput.pattern = "[0-9]{3}-*[0-9]{3}-*[0-9]{4}"): 
+            input == 'Email' ? formInput.type = 'email' : null;
+        formInput.required = true
+        form.appendChild(formInput);
+    })
+    const submit = document.createElement('input');
+    submit.type = 'submit';
+    form.appendChild(submit);
+
+
+
+    return form;
 }
 
 function createContact () {
     const content = document.getElementById('content');
     content.appendChild(heading());
     const subHeading = document.createElement('h2');
-    subHeading.innerText = 'Contact'
-    content.appendChild(subHeading)
+    subHeading.innerText = 'Contact';
+    content.appendChild(subHeading);
+    content.appendChild(contactForm());
 
 }
 
